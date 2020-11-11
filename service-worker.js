210edf7c810da1a9b77be11f1fa057debd69af74
samga <samgalaxy111111@gmail.com>
2020-11-11 16:19:57 +0700

    Promise, Service Worker, Cache API

---
+++ service-worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = "football-apps-v1";
+
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/detail-team.html",
+    "/nav.html",
+    "/manifest.json",
+    "/service-worker.js",
+    "/css/materialize.min.css",
+    "/css/style.css",
+    "/js/api.js",
+    "/js/materialize.min.js",
+    "/js/nav.js",
+    "/pages/standing.html",
+    "/pages/teams.html",
+    "/icon/icon-48.png",
+    "/icon/icon-96.png",
+    "/icon/icon-192.png",
+];
+
+self.addEventListener("install", function (event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    const base_url = "https://api.football-data.org/v2/";
+    if (event.request.url.indexOf(base_url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function(cache) {
+                return fetch(event.request).then(function(response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                })
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request, {'ignoreSearch': true}).then(function(response) {
+                return response || fetch (event.request);
+            })
+        )
+    }
+});
